4b33d3bceedcc92b35c33d477a9727a71c4b3c7b
Prashant <108861396+2prashant@users.noreply.github.com>
2024-01-18 14:04:38 +0000

    Arraylist

---
+++ Collection_FrameWork/Array_List.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+public class Array_List
+{
+    public static void main(String[] args)
+    {
+         ArrayList al1=new ArrayList();
+         al1.add(100);
+         al1.add(200);
+         al1.add(300);
+         System.out.println(al1);
+
+         ArrayList al2=new ArrayList();
+         al2.add("PW skills");
+         
+
+    }
+}
