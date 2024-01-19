025918532e565b00c65579941f888fafd03bddc8
Prashant <108861396+2prashant@users.noreply.github.com>
2024-01-19 07:59:32 +0000

    ArrayDeque

---
+++ Collection_FrameWork/Array_Deque.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Array_Deque 
+{
+    public static void main(String[] args) 
+    {
+        
+        ArrayDeque ad1=new ArrayDeque();
+        ad1.add(100);
+        ad1.add(200);
+        ad1.add(200);
+        System.out.println(ad1);
+        ad1.addFirst(10);
+        ad1.addLast(20);
+        System.out.println(ad1);
+
+        ad1.offer(500);
+        ad1.offerLast(510);
+        ad1.offerFirst(1);
+        System.out.println(ad1);
+
+        
+    }
+}
