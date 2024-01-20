b6b776f47b9a46082a16e5da0c818156ac134c2e
Prashant <108861396+2prashant@users.noreply.github.com>
2024-01-20 09:41:01 +0000

    map

---
+++ Map/map1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class map1 
+{
+    public static void main(String[] args) 
+    {
+        HashMap hm=new HashMap();
+        Hashtable th=new Hashtable();
+        LinkedHashMap lhm=new LinkedHashMap();
+        TreeMap tm=new TreeMap();
+    }    
+}
