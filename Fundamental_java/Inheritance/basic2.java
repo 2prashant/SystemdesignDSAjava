ee6501fe5eac2a5b036e1c27408836df9b3aeea9
Prashant <108861396+2prashant@users.noreply.github.com>
2024-02-18 05:42:35 +0000

    basic2 in Inheritance

--- Fundamental_java/Inheritance/basic2.java
+++ Fundamental_java/Inheritance/basic2.java
@@ -1,3 +1,35 @@
-public class basic2 {
-    
+class Human1
+{
+    private String name;
+    int age;
+    Human1()
+    {
+          System.out.println("Human class Constructor");;
+    }
+    void sleep()
+    {
+        age=18;
+        System.out.println("Human needs good sleep");
+        System.out.println(age);
+    }
+}
+class Student1 extends Human1
+{
+    // public Student1()
+    // {
+             //super();
+    // }
+    void disp()
+    {
+        System.out.println("the age is : "+age);
+        //System.out.println("THe Name is : "+name);// private
+    }
+}
+public class basic2
+{
+       public static void main(String[] args) 
+       {
+           Student1 s1=new Student1();
+           s1.disp();
+       }
 }
