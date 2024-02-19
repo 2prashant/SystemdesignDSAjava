6e6026dc8b94000d4fbf93662aa1fad00ed10340
Prashant <108861396+2prashant@users.noreply.github.com>
2024-02-19 04:05:43 +0000

    interface importance  show in basic 2

--- Interface/basic2.java
+++ Interface/basic2.java
@@ -1,24 +1,39 @@
-class Laptop
+interface  Computer
 {
-    public void compile()
+    void compilecode();
+    
+}
+class Laptop implements Computer
+{
+    public void compilecode()
     {
         System.out.println("You got 5 error");
     }
 }
+class Desktop implements Computer
+{
+      public void compilecode()
+      {
+          System.out.println("You got 5 errors, faster");
+      }
+}
 class Developer
 {
-    public void buildApp()
+    public void buildApp(Computer obj)
     {
        System.out.println("Building App");
+       obj.compilecode();
     }
-    Laptop obj=new Laptop();
-    obj.compilecode();
 }
+
 public class basic2 
 {
      public static void main(String[] args) 
      {
+      //  Laptop obj=new Laptop();
+      //   Desktop obj=new Desktop();
+          Computer obj=new Desktop();
          Developer dev=new Developer();
-         dev.buildApp();
+         dev.buildApp(obj);
      }
 }
