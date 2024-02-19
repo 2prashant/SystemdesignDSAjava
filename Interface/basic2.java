e7a3e7b0d711f4359ff6f047c6abcce85c475b24
Prashant <108861396+2prashant@users.noreply.github.com>
2024-02-19 03:27:39 +0000

    Interface

--- Interface/basic2.java
+++ Interface/basic2.java
@@ -1,3 +1,24 @@
-public class basic2 {
-    
+class Laptop
+{
+    public void compile()
+    {
+        System.out.println("You got 5 error");
+    }
+}
+class Developer
+{
+    public void buildApp()
+    {
+       System.out.println("Building App");
+    }
+    Laptop obj=new Laptop();
+    obj.compilecode();
+}
+public class basic2 
+{
+     public static void main(String[] args) 
+     {
+         Developer dev=new Developer();
+         dev.buildApp();
+     }
 }
