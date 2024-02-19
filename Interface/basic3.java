fc0156a8a4b267bfe9658d41c5e23c00cc658cd5
Prashant <108861396+2prashant@users.noreply.github.com>
2024-02-19 04:20:26 +0000

    interface3.2

--- Interface/basic3.java
+++ Interface/basic3.java
@@ -3,11 +3,21 @@ interface A
     int num=6;
     void show();
 }
-class B implements A
+interface x
+{
+    
+    void abc();
+    
+}
+class B implements A,x
 {
      public void show()
      {
           System.out.println(" in show ");
+     }
+     public void abc()
+     {
+        System.out.println("in abc");
      }
 }
 public class basic3 
