f06e916ae2b7244eed4bc08b55180a7b681192ed
Prashant <108861396+2prashant@users.noreply.github.com>
2024-02-19 04:14:31 +0000

    interface

--- Interface/basic3.java
+++ Interface/basic3.java
@@ -1,3 +1,23 @@
-public class basic3 {
-    
+interface A
+{
+    int num=6;
+    void show();
+}
+class B implements A
+{
+     public void show()
+     {
+          System.out.println(" in show ");
+     }
+}
+public class basic3 
+{
+    public static void main(String[] args)
+    {
+        
+        System.out.println(A.num);
+        B obj=new B();
+        obj.show();
+    }
+
 }
